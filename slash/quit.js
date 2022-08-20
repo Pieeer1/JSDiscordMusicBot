771312a3dc2c00f0bf7b64a99eb53cd58470e6b7
Pieeer1 <mason.adams@msn.com>
2022-08-20 15:00:46 -0700

    bug fix

--- slash/quit.js
+++ slash/quit.js
@@ -6,7 +6,7 @@ module.exports = {
 		const queue = client.player.getQueue(interaction.guildId)
 
 		if (!queue) return await interaction.editReply("There are no songs in the queue")
-
+		queue.clear()
 		queue.destroy()
         await interaction.editReply("Bye!")
 	},
